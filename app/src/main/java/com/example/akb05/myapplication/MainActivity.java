7baff686f786b3cd39721f2dd334881254ad21ce
ohdonghwan <38244619+ohdonghwan@users.noreply.github.com>
2018-04-10 19:05:54 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/akb05/myapplication/MainActivity.java
+++ app/src/main/java/com/example/akb05/myapplication/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     //First Commit Test
     
-      
+      //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
