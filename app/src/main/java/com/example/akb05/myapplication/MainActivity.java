3e0c2aa7302d24fd7ad2275a0ed8cdf8ae77bf6c
ohdonghwan <odh1993@ajou.ac.kr>
2018-04-10 19:13:08 +0900

    Third Commit Test in AndroidStudio

--- app/src/main/java/com/example/akb05/myapplication/MainActivity.java
+++ app/src/main/java/com/example/akb05/myapplication/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     //First Commit Test
     
     //Second Commit Test in Web
-    
+    //Third Commit Test in AndroidStudio
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
